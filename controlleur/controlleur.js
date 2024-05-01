1764468e4a616aa55a08b10862ee3a9cec2aba1c
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-01 13:42:17 -0400

    testing

--- controlleur/controlleur.js
+++ controlleur/controlleur.js
@@ -5,17 +5,17 @@ getall: async (req, res) => {
     console.log("controlleur pass");
     const auteur_id = req.params.id;
     console.log(auteur_id);
-
-    model.getAll(auteur_id)
-    .then(result => {
-        res.send(result);
-    })
-    .catch(error => {
-        console.error('Erreur lors de la récupération des tâches :', error);
-        res.status(500).json({ error: 'Erreur serveur' });
-    });
-
- 
+    try {
+        const tache = await model.getAll(auteur_id);
+        if (!tache) {
+            res.status(404).json({ error: `auteur: ${auteur_id} introuvable` });
+        } else {
+            res.json(tache);
+        }
+    } catch (error) {
+        console.error(`Error fetching tache with ID ${auteur_id}:`, error);
+        res.status(500).json({ error: 'erreur serveur interne' });
+    }
 }};
 
 module.exports = controlleur;
