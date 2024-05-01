1ff47d5d3f0bf7e2304889b5b2d7e17f1368deaa
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-01 10:34:11 -0400

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const pg = require('pg');
+
 const app = express();
 const PORT = 3000;
 app.use(express.json());
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send("<p>index</p>");
 });
 //route de base pour le reste des route
-app.use('/api/exam', require('./route/route').default);
+app.use('/api/exam', require('./route/route'));
 
 //garde le serveur ouvert
 app.listen(PORT, () => {
