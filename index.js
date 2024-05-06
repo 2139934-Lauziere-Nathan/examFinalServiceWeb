ee265bd6628b537e05abe35718c9cf4cf644e081
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-06 17:41:45 -0400

    update index

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("<p>index</p>");
 });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 //route de base pour le reste des route
 app.use('/api/exam', require('./route/route'));
 
