832f97c2cc642bd0b6c395a46d3f2337e0cb3f83
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-06 17:20:51 -0400

    update index

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ app.get('/', (req, res) => {
 });
 //route de base pour le reste des route
 app.use('/api/exam', require('./route/route'));
-var pageURL = './index.html';
-        
-// Redirect the browser to the specified page
-window.location.href = pageURL;
+
 //garde le serveur ouvert
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
