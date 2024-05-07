e7ae22df1e6bc27935c8997ae3dbe3a1f4e18d7c
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-07 09:44:31 -0400

    update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 app.use(express.json());
+/*
 app.get('/', (req, res) => {
     res.send("<p>index</p>");
 });
-
+*/
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
