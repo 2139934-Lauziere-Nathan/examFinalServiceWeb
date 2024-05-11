995fa53f38a2dc5e0f5befe6abb683523f5d513f
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-11 12:31:51 -0400

    update model

--- model/model.js
+++ model/model.js
@@ -197,7 +197,7 @@ const mod = {
             LIMIT 1;
             `
             const result = db.query(query);
-            return result.rows[0];
+            return result.rows;
         } catch (error) {
             throw error;
         }
