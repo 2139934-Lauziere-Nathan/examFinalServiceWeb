b249d6e5093885ce3eb2ef945b7933955b4a38a6
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-11 13:28:11 -0400

    update controlleur

--- controlleur/controlleur.js
+++ controlleur/controlleur.js
@@ -1,4 +1,5 @@
 const model = require('../model/model.js');
+const { route } = require('../route/route.js');
 
 const controlleur = {
      createUser: async (req, res) => {
@@ -13,7 +14,7 @@ const controlleur = {
             const newUser = await model.createUser(courriel, password );
             const retour = await model.getLastUser();
             console.log(retour);
-            res.status(201).json(newUser);
+            res.status(201).json(newUser, retour);
         } catch (error) {
 
             console.error("Error creating user:", error);
