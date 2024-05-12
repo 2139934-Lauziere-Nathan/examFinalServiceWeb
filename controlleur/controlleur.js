64ea7036c2648290d942c4c10db3c5dd10693017
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-11 23:01:26 -0400

    Update controlleur.js

--- controlleur/controlleur.js
+++ controlleur/controlleur.js
@@ -167,7 +167,8 @@ afficherDetail: async (req, res) => {
             const { tacheId, titre, complete } = req.body;
            
             const newSubTask = await model.ajouterSousTache(tacheId, titre, complete);
-            res.status(201).json({message : newSubTask});
+            console.log(newSubTask.titre);
+            res.status(201).json({message : newSubTask.titre});
         } catch (error) {
             console.error("erreur a l'ajout de la sous-tache:", error);
             res.status(500).json({ error: "erreur a l'ajout de la sous-tache" });
