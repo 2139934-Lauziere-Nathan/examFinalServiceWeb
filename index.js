d36350108969b2af6c1f4fbadd6108f0095c099f
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-05-13 16:14:55 -0400

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
 const PORT = 3001;
 // Dans votre fichier de démarrage
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
 //route de base pour le reste des route
 app.use('/api/exam', require('./route/route'));
 
